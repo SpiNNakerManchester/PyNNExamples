4de1166ceceda440196e023c7b1d54ebdbef8b30
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-07-03 12:05:27 +0100

    So much flake8...

--- examples/synfire_if_curr_exp_sEMD.py
+++ examples/synfire_if_curr_exp_sEMD.py
@@ -14,7 +14,7 @@ import matplotlib.pyplot as plt
 datum = datetime.datetime.now()
 
 step = 0.1
-p.setup(timestep = step)
+p.setup(timestep=step)
 n_neurons = 1
 run_time = 150
 cm = 0.25
@@ -26,28 +26,28 @@ v_reset = -85.0
 v_rest = -65.0
 v_thresh = -50.0
 
-weight= 1
+weight = 1
 delay1 = 0.1
 delay2 = 2.0
-												 # default parameters
-cell_params_lif = {'cm'        : cm, 			 # 1.0
-                   'i_offset'  : i_offset,		 # 0
-                   'tau_m'     : tau_m,			 # 20.0
-                   'tau_refrac': tau_refrac,	 # 0.1
-                   'tau_syn_E' : current_decay , # 5.0
-                   'tau_syn_I' : current_decay , # 5.0
-                   'v_reset'   : v_reset,		 # -65.0
-                   'v_rest'    : v_rest,		 # -65.0
-                   'v_thresh'  : v_thresh		 # -50.0
+
+cell_params_lif = {'cm'        : cm,
+                   'i_offset'  : i_offset,
+                   'tau_m'     : tau_m,
+                   'tau_refrac': tau_refrac,
+                   'tau_syn_E' : current_decay,
+                   'tau_syn_I' : current_decay,
+                   'v_reset'   : v_reset,
+                   'v_rest'    : v_rest,
+                   'v_thresh'  : v_thresh
                    }
 
 # neuron populations
 sEMD = p.Population(1, p.IF_curr_exp_sEMD(**cell_params_lif), label = "sEMD")
 spikeArray = {'spike_times': [[0]]}
 input_first = p.Population(1, p.SpikeSourceArray(**spikeArray),
-						 label = "input_first")
+						label = "input_first")
 input_second = p.Population(1, p.SpikeSourceArray(**spikeArray),
-						 label = "input_second")
+						label = "input_second")
 
 # projections
 p.Projection(input_first, sEMD,
@@ -66,10 +66,10 @@ sEMD.record(['v', 'gsyn_exc', 'gsyn_inh', 'spikes'])
 p.run(run_time)
 
 # get data
-spikes = sEMD.get_data(['spikes']) # read spikes
-v = sEMD.get_data(['v']) # read membrane voltage
-current_exc = sEMD.get_data(['gsyn_exc']) # read excitatory
-current_inh = sEMD.get_data(['gsyn_inh']) # read inhibitory
+spikes = sEMD.get_data(['spikes'])  # read spikes
+v = sEMD.get_data(['v'])  # read membrane voltage
+current_exc = sEMD.get_data(['gsyn_exc'])  # read excitatory
+current_inh = sEMD.get_data(['gsyn_inh'])  # read inhibitory
 
 print datum
 
@@ -79,8 +79,7 @@ Figure(
     Panel(spikes.segments[0].spiketrains,
           yticks=True, markersize=0.2, xlim=(0, run_time)),
     # membrane potential of the postsynaptic neuron
-    Panel(#v_test.segments[0].filter(name='v')[0],
-          v.segments[0].filter(name='v')[0],
+    Panel(v.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
           data_labels=[sEMD.label], yticks=True, xlim=(0, run_time)),
     Panel(current_exc.segments[0].filter(name='gsyn_exc')[0],
