5ab0ea6debaa85bc749f330747f42e8f04ac5e15
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-07-03 13:23:03 +0100

    Flake8 one more time

--- examples/synfire_if_curr_exp_sEMD.py
+++ examples/synfire_if_curr_exp_sEMD.py
@@ -32,26 +32,25 @@ delay2 = 2.0
 cell_params_lif = {'cm': cm, 'i_offset': i_offset, 'tau_m': tau_m,
                    'tau_refrac': tau_refrac, 'tau_syn_E': current_decay,
                    'tau_syn_I': current_decay, 'v_reset': v_reset,
-                   'v_rest': v_rest, 'v_thresh': v_thresh
-                  }
+                   'v_rest': v_rest, 'v_thresh': v_thresh}
 
 
 # neuron populations
-sEMD = p.Population(1, p.IF_curr_exp_sEMD(**cell_params_lif), label = "sEMD")
+sEMD = p.Population(1, p.IF_curr_exp_sEMD(**cell_params_lif), label="sEMD")
 spikeArray = {'spike_times': [[0]]}
 input_first = p.Population(1, p.SpikeSourceArray(**spikeArray),
-                           label = "input_first")
+                           label="input_first")
 input_second = p.Population(1, p.SpikeSourceArray(**spikeArray),
-                            label = "input_second")
+                            label="input_second")
 
 # projections
 p.Projection(input_first, sEMD,
              p.OneToOneConnector(),
-             receptor_type = "excitatory",
+             receptor_type="excitatory",
              synapse_type=p.StaticSynapse(weight=weight, delay=delay1))
 p.Projection(input_second, sEMD,
              p.OneToOneConnector(),
-             receptor_type = "inhibitory",
+             receptor_type="inhibitory",
              synapse_type=p.StaticSynapse(weight=weight, delay=delay2))
 
 # records
