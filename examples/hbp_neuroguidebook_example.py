8ed532fe1c55f42b00f64828590f561d8000511f
Andrew Gait <andrew.gait@manchester.ac.uk>
2017-10-23 11:53:16 +0100

    Flake8

--- examples/hbp_neuroguidebook_example.py
+++ examples/hbp_neuroguidebook_example.py
@@ -16,7 +16,7 @@ sim.setup(timestep=1.0, min_delay=1.0)
 cell_params = {
     'cm': 0.25, 'tau_m': 10.0, 'tau_refrac': 2.0,
     'tau_syn_E': 2.5, 'tau_syn_I': 2.5,
-    'v_reset': -70.0, 'v_rest': -65.0, 'v_thresh': -55.0 }
+    'v_reset': -70.0, 'v_rest': -65.0, 'v_thresh': -55.0}
 
 neurons = sim.Population(100, sim.IF_cond_exp(**cell_params))
 inputs = sim.Population(100, sim.SpikeSourcePoisson(rate=0.0))
@@ -26,7 +26,7 @@ input_firing_rates = np.linspace(0.0, 1000.0, num=inputs.size)
 inputs.set(rate=input_firing_rates)
 
 # create one-to-one connections
-wiring =  sim.OneToOneConnector()
+wiring = sim.OneToOneConnector()
 static_synapse = sim.StaticSynapse(weight=0.1, delay=2.0)
 connections = sim.Projection(inputs, neurons, wiring, static_synapse)
 
@@ -34,7 +34,7 @@ connections = sim.Projection(inputs, neurons, wiring, static_synapse)
 neurons.record('spikes')
 
 # run simulation
-sim_duration = 10.0 # seconds
+sim_duration = 10.0  # seconds
 sim.run(sim_duration*1000.0)
 
 # retrieve recorded data
