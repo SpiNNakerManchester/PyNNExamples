3f1325bb7f6b3dad8f52194d3df3bfb520def837
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-07-24 17:19:53 +0100

    Include different component amplitudes and correct axes labels

--- rat_barrel_cortex/fft_example.py
+++ rat_barrel_cortex/fft_example.py
@@ -8,22 +8,26 @@ t = np.arange(n)
 # Setup signal as combination of sin waves of different frequency
 input_freq_1 = 2
 input_freq_2 = 30
+amp_1 = 1
+amp_2 = 5
 
 for i in t:
-    x[i] = np.sin(input_freq_1 * i * (2 * np.pi / n)) \
-         + np.sin(input_freq_2 * i * (2 * np.pi / n))
+    x[i] = amp_1 * np.sin(input_freq_1 * i * (2 * np.pi / n)) \
+         + amp_2 * np.sin(input_freq_2 * i * (2 * np.pi / n))
 
 # Compute and normalise FT
 FT = np.fft.fft(x)/n
 
 # Plot signal and FT
 fig, ax = plt.subplots(2,1)
-ax[0].plot(t,x)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
+ax[0].plot(t,x)
+
 ax[1].plot(t[range(n/2)],abs(FT[range(n/2)])) # plot half as symmetric
-ax[0].set_xlabel('Frequency')
-ax[0].set_ylabel('|FT|')
+ax[1].set_xlabel('Frequency')
+ax[1].set_ylabel('|FT|')
+plt.subplots_adjust(hspace=0.4)
 plt.show()
 
 
