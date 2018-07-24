931ac5be1ed7a7d8fea9ae5a90fc6353795fcd87
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2018-07-24 17:34:45 +0100

    Include freq printing

--- rat_barrel_cortex/fft_example.py
+++ rat_barrel_cortex/fft_example.py
@@ -7,28 +7,38 @@ t = np.arange(n)
 
 # Setup signal as combination of sin waves of different frequency
 input_freq_1 = 2
-input_freq_2 = 30
+input_freq_2 = 25
 amp_1 = 1
-amp_2 = 5
+amp_2 = 3.75
 
 for i in t:
     x[i] = amp_1 * np.sin(input_freq_1 * i * (2 * np.pi / n)) \
          + amp_2 * np.sin(input_freq_2 * i * (2 * np.pi / n))
 
-# Compute and normalise FT
-FT = np.fft.fft(x)/n
+
+FT = np.fft.fft(x)/(n/2) # Compute and normalise FT
+FT = FT[range(n/2)] # FT is symmetric so only need half
+
+# Extract frequencies to print
+freqs = np.fft.fftfreq(n)
+
+# print abs(FT)
+print 'FT frequencies'
+for f in range(len(FT)):
+    if (abs(FT[f]) > 0.001):
+        print freqs[f]*n
 
 # Plot signal and FT
 fig, ax = plt.subplots(2,1)
 ax[0].set_xlabel('Time')
 ax[0].set_ylabel('Amplitude')
 ax[0].plot(t,x)
-
 ax[1].plot(t[range(n/2)],abs(FT[range(n/2)])) # plot half as symmetric
 ax[1].set_xlabel('Frequency')
 ax[1].set_ylabel('|FT|')
 plt.subplots_adjust(hspace=0.4)
 plt.show()
+
 
 
 
