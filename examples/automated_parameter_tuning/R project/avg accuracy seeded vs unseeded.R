89b0d5168451ea3d9fb29f3233887563d76a6120
ej159 <edwardgeorgejones@gmail.com>
2018-09-05 20:39:12 +0100

    MNIST full test results 227g

--- examples/automated_parameter_tuning/R project/avg accuracy seeded vs unseeded.R
+++ examples/automated_parameter_tuning/R project/avg accuracy seeded vs unseeded.R
@@ -3,8 +3,8 @@ library(ggrepel)
 
 datas <- rbind(cbind(generation=1:nrow(seeded), seeded, seeded=c("seeded")),cbind(generation=1:nrow(unseeded),unseeded, seeded=c("unseeded")))
 
-ggplot(datas[which(datas$generation<228),], aes(x=generation, y=avg, group=seeded)) +
-  scale_x_continuous(breaks=seq(0,230,10)) + 
+ggplot(datas[which(datas$generation<305),], aes(x=generation, y=avg, group=seeded)) +
+  scale_x_continuous(breaks=seq(0,305,10)) + 
   scale_y_continuous(labels = scales::percent, limits=c(0,1), breaks=seq(0,1,0.1)) +
   geom_line(aes(colour=seeded)) +
   geom_line(aes(y=max, colour=seeded)) +
@@ -15,7 +15,6 @@ ggplot(datas[which(datas$generation<228),], aes(x=generation, y=avg, group=seede
   theme_bw() +
   theme(legend.position=c(.1, .8)) +
   xlab("Generation") + ylab("Training Accuracy") +
-
   theme(legend.position=c(.1, .8)) +
   theme(legend.title = element_blank()) +
   theme(legend.background = element_rect(size=0.5, linetype="solid", colour ="black"))+
