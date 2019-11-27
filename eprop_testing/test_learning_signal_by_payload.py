09e63deedc1c5d8ec5775a9815d36ea12ea0d36a
oliverrhodes <oliver.rhodes@manchester.ac.uk>
2019-11-27 09:33:50 +0000

    add sinusoid to test script

--- eprop_testing/test_learning_signal_by_payload.py
+++ eprop_testing/test_learning_signal_by_payload.py
@@ -5,7 +5,7 @@ from pyNN.random import NumpyRNG, RandomDistribution
 from pyNN.utility.plotting import Figure, Panel
 
 
-runtime = 1000
+runtime = 1024
 pynn.setup(1.0)
 
 neuron_params = {
@@ -14,10 +14,17 @@ neuron_params = {
     "v_rest": 0
     }
 
+target_data = []
+for i in range(1024):
+            target_data.append(
+                5 + 2 * np.sin(2 * i * 2* np.pi / 1024) \
+                    + 2 * np.sin((4 * i * 2* np.pi / 1024))
+                )
+
 readout_neuron_params = {
     "v": 0,
     "v_thresh": 30, # controls firing rate of error neurons
-    "target_data": [0 for x in range(1024)],
+    "target_data": target_data,
     }
 
 
@@ -37,38 +44,48 @@ in_proj = pynn.Projection(input_pop,
                           receptor_type='excitatory')
 
 # Output population
-pop_out = pynn.Population(3, # HARDCODED 3: One readout; one exc err, one inh err
+readout_pop = pynn.Population(3, # HARDCODED 1
                        pynn.extra_models.SinusoidReadout(
                             **readout_neuron_params
-                           ),  # Neuron model
-                       label="pop_out" # identifier
+                           ), 
+                       label="readout_pop" 
                        )
-
 
 input_pop.record('spikes')
 neuron.record('all')
+readout_pop.record('all')
 
 pynn.run(runtime)
 in_spikes = input_pop.get_data('spikes')
 neuron_res = neuron.get_data('all')
+readout_res = readout_pop.get_data('all')
 
 Figure(
     Panel(in_spikes.segments[0].spiketrains,
           ylabel="Input Spikes",
           data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
-    Panel(neuron_res.segments[0].filter(name='v')[0],
+#     Panel(neuron_res.segments[0].filter(name='v')[0],
+#           ylabel="Membrane potential (mV)",
+#           data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
+#     Panel(neuron_res.segments[0].filter(name='gsyn_exc')[0],
+#           ylabel="gsyn excitatory (mV)",
+#           data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
+#     Panel(neuron_res.segments[0].filter(name='gsyn_inh')[0],
+#           xlabel="Time (ms)", xticks=True,
+#           ylabel="gsyn inhibitory (mV)",
+    Panel(readout_res.segments[0].filter(name='v')[0],
           ylabel="Membrane potential (mV)",
-          data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
-    Panel(neuron_res.segments[0].filter(name='gsyn_exc')[0],
+          data_labels=readout_pop.label, yticks=True, xlim=(0, runtime)),
+    Panel(readout_res.segments[0].filter(name='gsyn_exc')[0],
           ylabel="gsyn excitatory (mV)",
-          data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
-    Panel(neuron_res.segments[0].filter(name='gsyn_inh')[0],
+          data_labels=readout_pop.label, yticks=True, xlim=(0, runtime)),
+    Panel(readout_res.segments[0].filter(name='gsyn_inh')[0],
           xlabel="Time (ms)", xticks=True,
           ylabel="gsyn inhibitory (mV)",
-          data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
-    Panel(neuron_res.segments[0].spiketrains,
+          data_labels=readout_pop.label, yticks=True, xlim=(0, runtime)),
+    Panel(readout_res.segments[0].spiketrains,
           ylabel="Output Spikes",
-          data_labels=neuron.label, yticks=True, xlim=(0, runtime)),
+          data_labels=readout_pop.label, yticks=True, xlim=(0, runtime)),
     title="Single eprop neuron"
 )
 
