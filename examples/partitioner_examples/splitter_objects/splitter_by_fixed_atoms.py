21d962e112a0083029c122f71398413cc627692b
alan-stokes <alan.barry.stokes@gmail.com>
2020-08-20 10:48:31 +0100

    started work for examples

---
+++ examples/partitioner_examples/splitter_objects/splitter_by_fixed_atoms.py
@@ -0,0 +1,43 @@
+# Copyright (c) 2017-2019 The University of Manchester
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+from examples.partitioner_examples.splitter_objects.common_splitter import (
+    CommonSplitter)
+
+
+class SplitterByFixedAtom(CommonSplitter):
+
+    def __init__(self):
+        CommonSplitter.__init__(self)
+
+    def create_machine_vertices(self, resource_tracker, machine_graph):
+        pass
+
+    def get_out_going_slices(self):
+        pass
+
+    def get_in_coming_slices(self):
+        pass
+
+    def get_pre_vertices(self):
+        pass
+
+    def get_post_vertices(self):
+        pass
+
+    def machine_vertices_for_recording(self, variable_to_record):
+        pass
+
+    def can_support_delays_up_to(self, max_delay):
+        pass
