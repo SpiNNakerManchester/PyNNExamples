82ed314eb219b901166131a0b914c69a8404397a
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2023-01-10 07:08:55 +0000

    flake8

--- learning/simple.py
+++ learning/simple.py
@@ -21,7 +21,8 @@ sim.setup(timestep=1.0)
 sim.set_number_of_neurons_per_core(sim.IF_curr_exp, 100)
 
 pop_1 = sim.Population(1, sim.IF_curr_exp(), label="pop_1")
-input_pop = sim.Population(1, sim.SpikeSourceArray(spike_times=[0]), label="input")
+input_pop = sim.Population(
+    1, sim.SpikeSourceArray(spike_times=[0]), label="input")
 input_proj = sim.Projection(input_pop, pop_1, sim.OneToOneConnector(),
                             synapse_type=sim.StaticSynapse(weight=5, delay=1))
 pop_1.record(["spikes", "v"])
