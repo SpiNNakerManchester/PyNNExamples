afd0ed37bc31d3f9208ffb41072c77e61d14376f
Christian Y. Brenninkmeijer <christian.brenninkmeijer@manchester.ac.uk>
2026-02-10 07:24:43 +0000

    unused import

--- examples/extra_models_examples/vogels_2011.py
+++ examples/extra_models_examples/vogels_2011.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os
-from pyNN.random import NumpyRNG
 import pyNN.spiNNaker as sim
 import numpy
 import matplotlib.pyplot as pylab
